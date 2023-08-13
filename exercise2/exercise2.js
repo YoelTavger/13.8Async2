3bf4e907af24e6c11dc7e6aadad9b4275afd28c7
YoelTavger <yoel.tavger@gmail.com>
2023-08-13 18:20:08 +0300

    I completed Exercise 2

--- exercise2/exercise2.js
+++ exercise2/exercise2.js
@@ -1,6 +1,11 @@
 async function getJokes() {
-  const response = await fetch("https://api.humorapi.com/jokes/search");
-  const dataJokes = await response.json();
-  console.log(dataJokes);
+  const response = await fetch(
+    "https://api.humorapi.com/jokes/search?api-key=03bd5730078f42efac53ed4b90308e1d");
+  const data = await response.json();
+  return data.jokes[0].joke;
 }
-getJokes()
+getJokes().then((joke) => {
+  const div = document.createElement("div");
+  div.textContent = joke;
+  document.body.appendChild(div);
+});
